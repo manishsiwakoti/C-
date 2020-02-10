862d2d42825649ee136619cc2d3ec66eeb5f0aa9
max-student <max-student@maxtrain.com>
2020-02-10 15:19:20 -0500

    HomeWork

---
+++ JustPractice/Practice1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Practice1
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            Console.WriteLine("Hello World!");
+
+            var sum = 1;
+            for (var Index = 1; Index <= 10; Index++)
+                {
+                sum *= Index;
+                }
+
+            Console.WriteLine($"Sum is {sum}");
+
+            }
+        }
+        }
+    
