84564b07345c4c171a423c411aa9949f0c05a4fb
max-student <max-student@maxtrain.com>
2020-02-10 15:15:22 -0500

    StringTutorial

---
+++ StringTutorialProject1/StringTutorialProject1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringTutorialProject1
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            var st1 = new StringTutorial();
+           var Fullname = st1.Fullname();
+            Console.WriteLine($"Fullname is {Fullname}");
+
+            Console.WriteLine($"Fullname2 is {st1.Fullname2()}");
+
+            var denise = new StringTutorial("Denise", "Bartick");
+
+             Console.WriteLine($"Denise {denise.Fullname()}");
+            Console.WriteLine($"Denise {denise.Fullname2()}");
+            }
+        }
+    }
---
+++ StringTutorialProject1/StringTutorialProject1/StringTutorial.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringTutorialProject1
+    {
+    class StringTutorial
+        {
+
+        public string Firstname;
+        public string Lastname;
+
+
+        public StringTutorial() {
+
+            Firstname = "Manish";
+            Lastname = "Siwakoti";
+
+            }
+
+        public StringTutorial(String firstname, string lastname)  {
+            Firstname = firstname;
+            Lastname = lastname;
+
+            }
+
+        public string Fullname()
+            {
+            //var Firstname = "Manish";
+            //var Lastname = "Siwakoti";
+
+            // var Fullname = Firstname +" "+ Lastname;
+            var Fullname = $"{Lastname} {Firstname}";
+            return Fullname;
+
+         }
+
+        public string Fullname2() 
+            
+         {
+
+                //var Firstname = "Manish";
+                //var Lastname = "Siwakoti";
+
+                var Fullname = $"{Firstname} {Lastname}";
+                return Fullname;    
+
+
+
+
+         }
+            
+
+     }
+ }
+    
